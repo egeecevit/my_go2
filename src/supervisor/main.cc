40d390197b97cc7d8d8c81701cf920069d1485d1
Uluç Saranlı <saranli@ceng.metu.edu.tr>
2025-12-05 09:04:37 +0300

    adds safety module and localconf support to main, implements fallback to ~/rtrobot (#2)

--- src/supervisor/main.cc
+++ src/supervisor/main.cc
@@ -10,6 +10,8 @@
 #include <math.h>
 #include <signal.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <termios.h>
 
 #include "rtcore/Module.hh"
 #include "rtcore/ThreadUtil.hh"
@@ -29,6 +31,7 @@ void print_usage(const char* program_name) {
   printf("Usage: %s [OPTIONS]\n", program_name);
   printf("Options:\n");
   printf("  -c, --config CONFIG_STRING  Specify configuration string\n");
+  printf("  -n, --nosafety              Disable safety exit with the keyboard\n");
   printf("  -h, --help                  Show this help message and exit\n");
 }
 
@@ -42,9 +45,64 @@ void exit_on_ctrl_c(int) {
   _mgr->exitMainLoop();
 }
 
+// This module monitors the keyboard, if any key is hit, exits the
+// module manager loop. To this end, it reconfigures the terminal to
+// immediately issue all keyboard presses.
+class SafetyModule : public Module {
+
+public:
+  SafetyModule ( void ) : Module( "safety", 0, false ) {
+    peek = -1;
+    tcgetattr( 0, &origTerm );
+    newTerm = origTerm;
+    newTerm.c_lflag &= ~ICANON; //Disable canonical mode.(Do not buffer by line )
+    newTerm.c_lflag &= ~ECHO;   // Do not echo input characters
+    newTerm.c_cc[ VMIN ] = 1;
+    newTerm.c_cc[ VTIME ] = 0;
+    tcsetattr( 0, TCSANOW, &newTerm );
+  };
+  ~SafetyModule ( void ) {
+    tcsetattr( 0, TCSANOW, &origTerm );
+  }
+
+  int kbhit( void ) {
+    char ch;
+    int nread;
+
+    if( peek != -1 ) return 1;
+    newTerm.c_cc[ VMIN ] = 0;
+    tcsetattr( 0, TCSANOW, &newTerm );
+    nread = read( 0, &ch, 1 );
+    newTerm.c_cc[ VMIN ] = 1;
+    tcsetattr( 0, TCSANOW, &newTerm );
+    if ( nread == 1 ) {
+      peek = ch;
+      return 1;
+    }
+    return 0;
+  }
+    
+  void  init ( void ) { };
+  void  uninit ( void ) { };
+  void  activate ( void ) { };
+  void  deactivate ( void ) { };
+  void  update ( void ) {
+
+  if ( kbhit() ) {
+     _mgr->message( "User interrupt: Shutting down!" );
+     tcsetattr( 0, TCSANOW, &origTerm );
+     _mgr->exitMainLoop( );
+   }
+  }
+private:
+  struct termios origTerm, newTerm;
+  int peek;
+};
+
 int main( int argc, char **argv) {
 
   // Parse command line arguments
+  bool safety = true;
   std::string config_string;
   int option;
   struct option long_options[] = {
@@ -53,12 +111,15 @@ int main( int argc, char **argv) {
     {0, 0, 0, 0}
   };
   
-  while ((option = getopt_long(argc, argv, "c:h", long_options, nullptr)) != -1) {
+  while ((option = getopt_long(argc, argv, "nc:h", long_options, nullptr)) != -1) {
     switch (option) {
       case 'c':
         
         config_string += optarg;
         config_string += "\n";
+        break;
+      case 'n':
+        safety = false;
         break;
       case 'h':
         print_usage(argv[0]);
@@ -89,6 +150,9 @@ int main( int argc, char **argv) {
   res = mm.appendConfigFile("list.toml");
   res = res && mm.appendConfigFile("versionlist.toml");
   res = res && mm.appendConfigFile("robotlist.toml");
+  if (!mm.appendConfigFile("localconf.toml")) {
+    mm.warning("main", "Could not find localconf.toml, continuing.");
+  }
   res = res && mm.appendConfigString(config_string.c_str());
   if (!res) mm.fatalError( "main", "Could not find one or more configuration files!");
   if (! mm.finalizeConfig() )
@@ -103,6 +167,13 @@ int main( int argc, char **argv) {
   AddCoreModules( &mm );
   ActivateCoreModules( &mm );
 
+  SafetyModule *smod = nullptr;
+  if (safety) {
+    smod = new SafetyModule;
+    mm.addModule( smod, 10, 0, OTHER_MODULES );
+    mm.activateModule( smod );
+  }
+  
   // This activates the supervisor, which in turn activates other modules
   Supervisor *sm = new Supervisor;
   mm.addModule(sm, 1, 0, USER_CONTROLLERS);
