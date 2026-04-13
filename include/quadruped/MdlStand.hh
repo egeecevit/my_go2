124b26018b7df21b888990059c42d96a7ce0000c
Burak <burak2844@gmail.com>
2026-04-13 15:50:29 +0300

    add MdlStand header with activation-frame interface

---
+++ include/quadruped/MdlStand.hh
@@ -0,0 +1,47 @@
+#ifndef MDLSTAND_HH
+#define MDLSTAND_HH
+
+#include "rtcore/Module.hh"
+#include <Eigen/Dense>
+
+#define STANDMODULE_NAME "MdlStand"
+
+class MdlLegControl;
+
+class MdlStand : public rtcore::Module {
+public:
+  enum Status { IDLE, ACTIVE, SETTLED, ERROR };
+
+  MdlStand();
+  ~MdlStand();
+
+  void init();
+  void uninit();
+  void activate();
+  void deactivate();
+  void update();
+
+  void setTargetHeight(double delta_h, double duration);
+  void setTargetHeight(double delta_h);  // uses config duration
+  Status getStatus() const { return _status; }
+
+private:
+  MdlLegControl *_legs[4] = {};
+  Status _status = IDLE;
+
+  Eigen::Vector3d _footB0[4];
+  bool _footCaptured = false;
+
+  double _deltaH = 0.0;
+  double _duration = 2.0;
+  double _startTime = 0.0;
+  bool _trajectoryActive = false;
+
+  double _trackingErrorLimit = 0.5;
+
+  static double _quintic(double tau);
+  static double _quinticDot(double tau);
+  bool _checkTrackingError() const;
+};
+
+#endif
